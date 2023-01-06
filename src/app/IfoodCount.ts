37aa03d4e687090b8be3c89c63dbb39af39f483f
Jakub Kruliczak <jakubkruliczak@gmail.com>
2023-01-06 01:15:54 +0100

    added remove event

---
+++ src/app/IfoodCount.ts
@@ -0,0 +1,5 @@
+export interface IfoodCounts {
+  Name: string;
+  Price: number;
+  Count: number;
+}
--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Food } from '../food';
+import { IfoodCounts } from '../IfoodCount';
 import { listOfFoods } from '../food-compose';
 @Component({
   selector: 'app-food',
@@ -8,6 +9,39 @@ import { listOfFoods } from '../food-compose';
 })
 export class FoodComponent {
   foods: Food[] = listOfFoods;
+  listFood: Food[] = [];
+
+  cena: number = 0;
+  public Add(f: Food): void {
+    // let n: number = this._CountAdded(f);
+    // let rawF: IfoodCounts = {
+    //   Name: f.Name,
+    //   Price: f.Price,
+    //   Count: 0,
+    // };
+    this.listFood.push(f);
+    this.cena += f.Price;
+  }
+  // private _CountAdded(f: Food): number {
+  //   let n: number = 0;
+  //   this.listFood.forEach((element) => {
+  //     if (f.Name == element.Name) n++;
+  //   });
+  //   return n;
+  // }
+  public Remove(f: Food): void {
+    // let rawF: IfoodCounts = { Name: f.Name, Price: f.Price, Count: 0 };
+    this.listFood.splice(this.listFood.indexOf(f), 1);
+    this._Prices();
+  }
+  private _Prices(): void {
+    let price: number = 0;
+    this.listFood.forEach((element) => {
+      price += element.Price;
+    });
+    this.cena = price;
+  }
 }
 
 // "builder": "@angular-devkit/build-angular:dev-server",
+// "builder": "docs",
